832af69a9e2cab8ec70f30f84808b529c0e5bdcf
FreePhoenix888 <freephoenix888@gmail.com>
2023-05-14 14:53:45 +0600

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import { useDeepSubscription } from '@deep-foundation/deeplinks/imports/client';
+import { useState, useEffect } from 'react';
+
+export function useArePackagesInstalled(param: UseIsPackageInstalledParam) {
+  const { packageNames, shouldIgnoreResultWhenLoading = false, onError } = param;
+  const [packageInstallationStatuses, setPackageInstallationStatuses] = useState<PackageInstallationStatuses>(undefined);
+  const { data, loading, error } = useDeepSubscription({
+    type_id: {
+      _id: ['@deep-foundation/core', 'Package'],
+    },
+    string: {
+      value: {
+        _in: packageNames
+      },
+    },
+  });
+  useEffect(() => {
+    if (shouldIgnoreResultWhenLoading && loading) {
+      return;
+    }
+    if (error) {
+      onError?.({ error });
+      setPackageInstallationStatuses(undefined);
+      return;
+    }
+    let packageInstallationStatuses: PackageInstallationStatuses = {};
+    packageInstallationStatuses = packageNames.reduce<PackageInstallationStatuses>((packageInstallationStatuses, packageName) => {
+      packageInstallationStatuses!.packageName = !!(data && data.find(item => item.value?.value === packageName));
+      return packageInstallationStatuses;
+    }, packageInstallationStatuses);
+    setPackageInstallationStatuses(packageInstallationStatuses);
+  }, [data, loading, error]);
+
+  return { isPackageInstalled: packageInstallationStatuses, loading, error };
+}
+
+
+
+export interface UseIsPackageInstalledParam {
+  packageNames: Array<string>;
+  shouldIgnoreResultWhenLoading?: boolean;
+  onError?: ({ error }: { error: { message: string } }) => void;
+}
+
+export type PackageInstallationStatuses = Record<string, boolean|undefined> | undefined;
