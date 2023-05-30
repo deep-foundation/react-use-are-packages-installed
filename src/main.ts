d9b682d1f24e5f5267c667ad7cfcb39e0ce3258e
FreePhoenix888 <freephoenix888@gmail.com>
2023-05-30 21:45:41 +0600

    Fix  infinite loop

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,42 @@
-import { useDeepSubscription } from '@deep-foundation/deeplinks/imports/client';
-import { useState, useEffect } from 'react';
+import {
+  UseDeepSubscriptionResult,
+  useDeepSubscription,
+} from '@deep-foundation/deeplinks/imports/client';
+import { useState, useEffect, useMemo, useRef } from 'react';
 
 export function useArePackagesInstalled(param: UseArePackagesInstalledParam) {
-  const { packageNames, shouldIgnoreResultWhenLoading = false, onError } = param;
-  const [packageInstallationStatuses, setPackageInstallationStatuses] = useState<PackageInstallationStatuses>(undefined);
+  const { packageNames } = param;
+
   const { data, loading, error } = useDeepSubscription({
     type_id: {
       _id: ['@deep-foundation/core', 'Package'],
     },
     string: {
       value: {
-        _in: packageNames
+        _in: packageNames,
       },
     },
   });
-  useEffect(() => {
-    if (shouldIgnoreResultWhenLoading && loading) {
-      return;
-    }
-    if (error) {
-      onError?.({ error });
-      setPackageInstallationStatuses(undefined);
-      return;
-    }
-    let packageInstallationStatuses: PackageInstallationStatuses = {};
-    packageInstallationStatuses = packageNames.reduce<PackageInstallationStatuses>((packageInstallationStatuses, packageName) => {
-      packageInstallationStatuses![packageName] = !!(data && data.find(item => item.value?.value === packageName));
-      return packageInstallationStatuses;
-    }, packageInstallationStatuses);
-    setPackageInstallationStatuses(packageInstallationStatuses);
-  }, []);
+
+  let packageInstallationStatuses: PackageInstallationStatuses = undefined;
+  if (data) {
+    packageInstallationStatuses =
+      packageNames.reduce<PackageInstallationStatuses>(
+        (packageInstallationStatuses, packageName) => {
+          packageInstallationStatuses![packageName] = !!data.find(
+            (item) => item.value?.value === packageName
+          );
+          return packageInstallationStatuses;
+        },
+        packageInstallationStatuses ?? {}
+      );
+  }
 
   return { packageInstallationStatuses, loading, error };
 }
 
-
-
 export interface UseArePackagesInstalledParam {
   packageNames: Array<string>;
-  shouldIgnoreResultWhenLoading?: boolean;
-  onError?: ({ error }: { error: { message: string } }) => void;
 }
 
 export type PackageInstallationStatuses = Record<string, boolean> | undefined;
