54a16e1eff84ea430d1f5c2693ccc8a4223997c9
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-02 19:30:52 +0600

    Add transformIgnorePatterns

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,9 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  transformIgnorePatterns: [
+    "node_modules/"
+  ],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
