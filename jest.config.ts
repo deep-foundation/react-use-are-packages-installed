707764f008fb12fd24958b53a2052041bc4c5d39
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-02 20:49:43 +0600

    Update testEnvironmentOptions.url

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@ import type { Config } from 'jest'
 const jestConfig: Config = {
   testEnvironment: 'jsdom',
   verbose: true,
+  testEnvironmentOptions: {
+    url: "http://localhost/"
+  }
 }
 
 export default jestConfig
