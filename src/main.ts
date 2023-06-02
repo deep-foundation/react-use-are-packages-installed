88f4cfe820b1cf5f94f3e780a09684d2d8024377
flakeed <vova.krivosheya.2004@gmail.com>
2023-06-02 23:23:26 +0000

    Add code with passed tests

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import {
-  DeepClient,
+  UseDeepSubscriptionResult,
   useDeepSubscription,
-} from '@deep-foundation/deeplinks/imports/client.js';
-import { useMemo } from 'react';
+} from '@deep-foundation/deeplinks/imports/client';
+import { useState, useEffect, useMemo, useRef } from 'react';
 
 export function useArePackagesInstalled(param: UseArePackagesInstalledParam) {
-  const { packageNames, deep } = param;
+  const { packageNames } = param;
 
-  const { data, loading, error } = deep.useDeepSubscription({
+  const { data, loading, error } = useDeepSubscription({
     type_id: {
       _id: ['@deep-foundation/core', 'Package'],
     },
@@ -18,28 +18,25 @@ export function useArePackagesInstalled(param: UseArePackagesInstalledParam) {
     },
   });
 
-  const packageInstallationStatuses: PackageInstallationStatuses | undefined = useMemo(() => {
-    if (!data) {
-      return undefined;
-    } else {
-      return packageNames.reduce<PackageInstallationStatuses>(
-        (acc, packageName) => {
-          acc[packageName] = !!data.find(
+  let packageInstallationStatuses: PackageInstallationStatuses = undefined;
+  if (data) {
+    packageInstallationStatuses =
+      packageNames.reduce<PackageInstallationStatuses>(
+        (packageInstallationStatuses, packageName) => {
+          packageInstallationStatuses![packageName] = !!data.find(
             (item) => item.value?.value === packageName
           );
-          return acc;
+          return packageInstallationStatuses;
         },
-        {} as PackageInstallationStatuses
+        packageInstallationStatuses ?? {}
       );
-    }
-  }, [data]);
+  }
 
   return { packageInstallationStatuses, loading, error };
 }
 
 export interface UseArePackagesInstalledParam {
-  deep: DeepClient;
   packageNames: Array<string>;
 }
 
-export type PackageInstallationStatuses = Record<string, boolean>;
+export type PackageInstallationStatuses = Record<string, boolean> | undefined;
