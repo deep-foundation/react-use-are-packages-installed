4f8cc454e062ecc7d9ae79336afc131d562e3d2d
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-03 12:31:24 +0600

    Update tests

--- tests/main.tsx
+++ tests/main.tsx
@@ -1,8 +1,15 @@
 import dotenv from 'dotenv';
-import { DeepClient, DeepProvider } from '@deep-foundation/deeplinks/imports/client.js';
+import {
+  DeepClient,
+  DeepProvider,
+  UseDeepSubscriptionResult,
+} from '@deep-foundation/deeplinks/imports/client.js';
 import { generateApolloClient } from '@deep-foundation/hasura/client.js';
 import { render, waitFor } from '@testing-library/react';
-import { useArePackagesInstalled } from '../src/main';
+import {
+  UseArePackagesInstalledResult,
+  useArePackagesInstalled,
+} from '../src/main';
 import { ApolloProvider } from '@apollo/client/index.js';
 import { assert } from 'chai';
 import React from 'react';
@@ -17,37 +24,46 @@ requiredEnvNames.forEach((name) => {
 });
 
 const path = process.env.GRAPHQL_PATH!;
-console.log("graphQlPath", path)
+console.log('graphQlPath', path);
 
 const token = process.env.TOKEN!;
-console.log("token", token)
+console.log('token', token);
 
-const packageNames = process.env.PACKAGE_NAMES!.split(',');
+const packageNames = process.env.PACKAGE_NAMES!.split(',').map((name) => name.trim());
 const timeout = process.env.TIMEOUT ? parseInt(process.env.TIMEOUT) : 10000;
 
 const apolloClient = generateApolloClient({
   path,
   ssl: true,
   ws: true,
-  token
+  token,
 });
-console.log("apolloclient", apolloClient)
+let deep = new DeepClient({ apolloClient });
+
+beforeAll(async () => {
+  await deep.whoami();
+  assert.notEqual(deep.linkId, 0);
+  assert.notEqual(deep.linkId, undefined);
+  const {data: installedPackageLinks} = await deep.select({
+    type_id: {
+      _id: ['@deep-foundation/core', 'Package'],
+    },
+    string: {
+      value: {
+        _in: packageNames,
+      },
+    },
+  });
+  const installedPackageNames = new Set(installedPackageLinks.map((link: any) => link.value.value))
+  assert.equal(installedPackageNames.size, packageNames.length, 'Number of installed packages is not equal to number of packages requested');
+});
 
 describe('main', () => {
   it('installed packages', async () => {
-
-    const deep = new DeepClient({ apolloClient });
-    await deep.whoami();
-
-    console.log("deep.linkid", deep.linkId)
-
-    assert.notEqual(deep.linkId, 0);
-    assert.notEqual(deep.linkId, undefined);
-
-    let testResult;
+    let deepSubscriptionResult: UseArePackagesInstalledResult;
     const TestComponent = () => {
-      testResult = useArePackagesInstalled({ packageNames });
-      console.log("testresult", testResult)
+      deepSubscriptionResult = useArePackagesInstalled({ deep, packageNames });
+      console.log('testresult', deepSubscriptionResult);
       return null;
     };
 
@@ -61,10 +77,26 @@ describe('main', () => {
 
     await waitFor(
       () => {
-        expect(testResult.loading).toBe(false);
-        expect(testResult.error).toBe(undefined);
-        expect(testResult.packageInstallationStatuses).not.toBe(undefined);
-        expect(Object.keys(testResult.packageInstallationStatuses)).toEqual(packageNames);
+        assert.equal(deepSubscriptionResult.loading, false, 'Loading is true');
+        assert.equal(deepSubscriptionResult.error, undefined, 'Error is not undefined');
+        assert.notEqual(
+          deepSubscriptionResult.packageInstallationStatuses,
+          undefined,
+          'Package installation statuses is undefined'
+        );
+        assert.equal(
+          Object.keys(deepSubscriptionResult.packageInstallationStatuses).length,
+          packageNames.length,
+          'Number of packages installed is not equal to number of packages requested'
+        );
+        Object.entries(deepSubscriptionResult.packageInstallationStatuses).forEach(
+          ([packageName, status]) => {
+            assert.isTrue(
+              status,
+              `${packageName} installation status is false`
+            );
+          }
+        );
       },
       {
         timeout,
